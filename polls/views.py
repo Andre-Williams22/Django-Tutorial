6e4171923836f0611904a3e4f8249a8503d37e97
Andre Williams <awjrs22@gmail.com>
2020-04-28 21:41:48 -0700

    part 5

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect 
-from django.template import loader 
-from django.urls import reverse 
-from django.views import generic 
-from django.utils import timezone
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
 from django.urls import reverse
-import datetime
-from .models import Choice, Question
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question, Choice
 
 
 def index(request):
@@ -45,12 +44,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """ Return the last five published questions (not including those set to be published in the future)."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-def get_queryset(self):
-    """ Return the last five published questions (not including those set to be published in the future)."""
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -67,33 +67,3 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-
-def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
-# Code with Functions 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-    
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question':question})
